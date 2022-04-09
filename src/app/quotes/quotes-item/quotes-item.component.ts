5976268a081b1b44426e9d9075bb09ded5ab568d
Joy Christine <joychristin2@gmail.com>
2022-04-09 10:09:24 +0300

    initial commit

--- src/app/quotes/quotes-item/quotes-item.component.ts
+++ src/app/quotes/quotes-item/quotes-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from 'src/app/quotes.model';
 
 @Component({
   selector: 'app-quotes-item',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-item.component.css']
 })
 export class QuotesItemComponent implements OnInit {
-
+  quotes : Quotes[] =[
+    new Quotes('an apple a day keeps a doctor away','doctor mike')
+  ];
   constructor() { }
 
   ngOnInit(): void {
