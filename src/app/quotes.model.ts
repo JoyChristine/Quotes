605945034629c5f09f885fdaca2b18f850987ca8
Joy Christine <joychristin2@gmail.com>
2022-04-10 10:40:42 +0300

    initial commit

--- src/app/quotes.model.ts
+++ src/app/quotes.model.ts
@@ -1,3 +1,4 @@
 export class Quotes {
+    showInfo: boolean = false;
     constructor(public quote:string,public author:string,public upvote:number,public downvote:number,public name:string,public completeDate: Date) {}
 }
