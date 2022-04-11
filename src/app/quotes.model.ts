ad20c8ddcd0476c316e98d06c669644698b6040f
Joy Christine <joychristin2@gmail.com>
2022-04-11 12:24:38 +0300

    edit constructor - add completeDate

--- src/app/quotes.model.ts
+++ src/app/quotes.model.ts
@@ -1,4 +1,8 @@
 export class Quotes {
+    push(event: Quotes) {
+      throw new Error('Method not implemented.');
+    }
+    
     showInfo: boolean = false;
     constructor(public quote:string,public author:string,public upvote:number,public downvote:number,public name:string,public completeDate: Date) {}
 }
