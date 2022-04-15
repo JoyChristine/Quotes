beede27366a07004ed7137501449d5cfa0bd8096
Joy Christine <joychristin2@gmail.com>
2022-04-15 11:29:45 +0300

    Auto-generated commit [ci skip]

--- src/app/quotes.model.ts
+++
@@ -1,8 +0,0 @@
-export class Quotes {
-    push(event: Quotes) {
-      throw new Error('Method not implemented.');
-    }
-    
-    showInfo: boolean = false;
-    constructor(public quote:string,public author:string,public upvote:number,public downvote:number,public name:string,public completeDate: Date) {}
-}
